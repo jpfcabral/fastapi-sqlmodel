342480f060fe232d59fed2b4367bb89762018dee
jpfcabral <jpfcabral@gmail.com>
2022-03-03 10:55:11 -0300

    update: logging formatter

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, Session, create_engine
 sqlite_file_name = "../sqlite.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url)
 
 def create_database():
     SQLModel.metadata.create_all(engine)
