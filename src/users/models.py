87b12b1c2505d867a817e577d72b46e83534b237
jpfcabral <jpfcabral@gmail.com>
2022-03-03 18:06:41 -0300

    add: user email data

--- src/users/models.py
+++ src/users/models.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from typing import Optional
 from sqlmodel import Field, SQLModel
+from sqlalchemy import Column, String
 
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str
+    username: str  = Field(sa_column=Column("username", String, unique=True))
+    email: str = Field(sa_column=Column("email", String, unique=True))
     hashed_password: str
     active: Optional[bool] = Field(default=True)
     super_user: Optional[bool] = Field(default=False)
@@ -14,6 +16,7 @@ class User(SQLModel, table=True):
 
 class UserCreate(SQLModel):
     username: str
+    email: str
     password: str
 
 
