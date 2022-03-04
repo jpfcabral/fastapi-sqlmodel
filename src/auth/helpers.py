77acb0c61ef281cc21980dc26b6cf32f459724f9
jpfcabral <jpfcabral@gmail.com>
2022-03-04 12:20:32 -0300

    add: jwt token

--- src/auth/helpers.py
+++ src/auth/helpers.py
@@ -1,8 +1,12 @@
 import bcrypt
+import jwt
 from fastapi import Depends
 from sqlmodel import Session
 from database import get_session
 from users.crud import read_user
+from users.models import User, UserRead
+from config import settings
+from auth.models import Token
 
 
 def authenticate(username: str, password: str,  db: Session = Depends(get_session)):
@@ -11,3 +15,10 @@ def authenticate(username: str, password: str,  db: Session = Depends(get_sessio
         return bcrypt.checkpw(password.encode('utf8'), user.hashed_password.encode('utf8'))
     else:
         return False
+
+def create_token(user: User):
+    user_obj = UserRead.from_orm(user)
+    token = jwt.encode(user_obj.dict(), settings.SECRET_KEY, settings.ALGORITHM)
+    response = Token(access_token=token, token_type='bearer')
+
+    return response.dict()
--- src/auth/views.py
+++ src/auth/views.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodel import Session
 
-from auth.helpers import authenticate
+from auth.helpers import authenticate, create_token
 from database import get_session
+from users.crud import read_user
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/token')
@@ -13,9 +14,11 @@ router = APIRouter()
 @router.post('/token')
 async def generate_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_session)):
     if not authenticate(form_data.username, form_data.password, db=db):
-        raise HTTPException(status_code=402, detail="Invalid Credentials")
-        
-    return {'access_token':form_data.username, 'token_type':'bearer'}
+        raise HTTPException(status_code=401, detail="Invalid Credentials")
+    
+    user = read_user(form_data.username, db)
+
+    return create_token(user=user)
 
 @router.get('/')
 def check_auth(token: str = Depends(oauth2_scheme)):
