ccb87055137c5fdfb16d9f8f017341d0c8238e2c
jpfcabral <jpfcabral@gmail.com>
2022-03-04 12:42:14 -0300

    add: get current user

--- src/auth/helpers.py
+++ src/auth/helpers.py
@@ -1,6 +1,6 @@
 import bcrypt
 import jwt
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlmodel import Session
 from database import get_session
 from users.crud import read_user
@@ -22,3 +22,12 @@ def create_token(user: User):
     response = Token(access_token=token, token_type='bearer')
 
     return response.dict()
+
+def get_current_user(token: str, db: Session = Depends(get_session)):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+        user = read_user(username=payload['username'], db=db)
+    except Exception as e:
+        raise HTTPException(status_code=401, detail='Invalid Credentials')
+    
+    return UserRead.from_orm(user)
--- src/auth/views.py
+++ src/auth/views.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodel import Session
 
-from auth.helpers import authenticate, create_token
+from auth.helpers import authenticate, create_token, get_current_user
 from database import get_session
 from users.crud import read_user
+from users.models import UserRead
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/token')
@@ -20,6 +21,7 @@ async def generate_token(form_data: OAuth2PasswordRequestForm = Depends(), db: S
 
     return create_token(user=user)
 
-@router.get('/')
-def check_auth(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+@router.get('/', response_model=UserRead)
+def check_auth(token: str = Depends(oauth2_scheme), db: Session = Depends(get_session)):
+    print(token)
+    return get_current_user(token=token, db=db)
