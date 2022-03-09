ee5699fc6ce7c056f22dc48905557615de20386d
jpfcabral <jpfcabral@gmail.com>
2022-03-09 18:33:38 -0300

    add: simple test

---
+++ tests/user_test.py
@@ -0,0 +1,14 @@
+from ssl import _PasswordType
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_create_user():
+    response = client.post('/users', data={
+            "username": "test",
+            "email": "test@gmail.com",
+            "password": "mysecret"
+        })   
+    assert response.status_code == 201
+    assert response.json() != {}
